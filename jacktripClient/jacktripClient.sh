1cbe5c29be82cc87274d73ef086342f2ee79c373
Kasper Jordaens <kasper.jordaens@imec.be>
2020-12-08 17:20:37 +0100

    calling it a day

---
+++ jacktripClient/jacktripClient.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Kasper Jordaens
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the BSD license.  See the LICENSE file for details.
+#
+
+echo "starting Jacktrip Client in HUB mode"
+nohup jacktrip -C choirbox1 -n 8
+
---
+++ jacktripServer/jacktripServer.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Kasper Jordaens
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the BSD license.  See the LICENSE file for details.
+#
+
+echo "kill all jacktrip Servers & Clients"
+killall jacktrip
+echo "starting Jacktrip Server in HUB mode"
+/usr/bin/jacktrip -S 
