32faaf2e5e2771d62a3fb0fc334b0aa2964e1da2
Wouter Devriese <wouter.devriese@imec.be>
2021-04-14 10:52:48 +0200

    added colouring to install script for better readability

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#colouring for easier readability
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
 read -p "What's the new device name? " devicename
 sudo hostname $devicename
@@ -11,11 +15,11 @@ sudo apt-get update && sudo apt-get upgrade -y
 cd
 
 #check if jacktrip repo exists already, otherwise clone it
-echo "installing jacktrip"
 if [ ! -d "jacktrip" ]; then
+    echo "{green}installing jacktrip{reset}"
     git clone https://github.com/jacktrip/jacktrip.git
 else
-    echo "update jacktrip"
+    echo "{green}update jacktrip{reset}"
     cd jacktrip
     git pull
     cd
@@ -25,11 +29,11 @@ cd jacktrip/src && ./build
 cd ../builddir && sudo make install
 cd
 
-echo "installing jmess"
 if [ ! -d "jmess-jack" ]; then
+    echo "{green}installing jmess{reset}"
     git clone https://github.com/jacktrip/jmess-jack.git
 else
-    echo "update jmess"
+    echo "{green}update jmess{reset}"
     cd jmess-jack
     git pull
     cd
@@ -40,7 +44,7 @@ cd
 
 if [ -d "choirbox" ]; then
     cd choirbox
-    echo "copying the necessary scripts"
+    echo "{green}copying the necessary scripts{reset}"
     echo "alias jackclient='$(pwd)/scripts/startup/jacktripClient/jacktripClient.sh'" >>/home/patch/.bash_aliases
     echo "alias jackserver='$(pwd)/scripts/startup/jacktripServer/jacktripServer.sh'" >>/home/patch/.bash_aliases
     echo "alias jackpatch='$(pwd)/scripts/startup/patching/patch.sh'" >>/home/patch/.bash_aliases
@@ -49,7 +53,7 @@ if [ -d "choirbox" ]; then
     sudo cp -r ./pisound_pure_data_modules/* /usr/local/puredata-patches/
 
 else
-    echo "Error: Install must be executed from the git repo choirbox (with this exact name)"
+    echo "{red}Error: Install must be executed from the git repo choirbox (with this exact name){reset}"
 fi
 
 sudo reboot
